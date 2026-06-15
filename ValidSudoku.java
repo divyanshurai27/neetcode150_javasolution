7977ce92b21cd2ff8e3c7fc27f055bc6ffd8c7f7
divyanshurai27 <146620517+divyanshurai27@users.noreply.github.com>
2026-06-15 14:12:46 +0530

    Add Valid Sudoku solution

---
+++ ValidSudoku.java
@@ -0,0 +1,25 @@
+import java.util.HashSet;
+
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        HashSet<String> set = new HashSet<>();
+
+        for (int r = 0; r < 9; r++) {
+            for (int c = 0; c < 9; c++) {
+                char num = board[r][c];
+                if (num == '.') {
+                    continue;
+                }
+
+                String row = num + "in row" + r;
+                String col = num + "in col" + c;
+                String box = num + "in box" + r / 3 + "-" + c / 3;
+
+                if (!set.add(row) || !set.add(col) || !set.add(box)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
